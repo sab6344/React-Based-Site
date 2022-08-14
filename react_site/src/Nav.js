b15d67cadf9b8e9d914c00fc98535ab129203f51
sab6344 <sab6344@psu.edu>
2022-08-14 09:04:02 -0400

    asd
    dad

---
+++ react_site/src/Nav.js
@@ -0,0 +1,16 @@
+import  React from 'react';
+import './Nav.css';
+function NavBar()
+{
+    return(
+
+    <div className="Nav">
+        <div className="Links">
+            <h2> Home</h2>
+            <h2>  free</h2>
+            <h2> full</h2>
+        </div>
+    </div>
+    )
+}
+export default NavBar;
